b33bceb3844b2c6433f8b90a9796a5c4144f23de
SachinDatir <sdatir83@gmail.com>
2022-11-11 12:38:32 +0530

    message

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// why git?
+// local machine   ====> repo repository(Github)
+// version control ====>  f1 ===> f2 
+// Code merge      ====>  Differnt users
+// Security        ====>  if local machine is not available
+
+// pr)git config --global user.name "minskoleopc"
+// pr)git config --global user.email "minskoleopc@gmail.com"
+
+// 1) git download and install 
+// 2) git hub (username , email , password)
+// 3) Create a folder on Desktop
+// 4) Add a file open a terminal and run git status
+// 5) git init   (working directory ====> staging ====> localR  ====> remote R)
+// 6) git add . / git add script.js
+// 7) git commit -m "myFirstCommit"
+// 8) create repository on gitHub and copy the last 3 commands
+// 9) Check the git repo
+// 10)git branch -M main
+// 11)git remote add origin https://github.com/Minskoleopc/firstMs.git
+// 12)git push -u origin main
+// 13)git pull -u origin main
+// 14)git branch
+// 15)git branch aone
+// 16)git checkout aone
+// 17)add and commit on new branch and push the branch to remote
+// 18)create pull request and merge 
+// 19)git branch -d aone
